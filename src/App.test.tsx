b74ce9e38480303a59cac4705536142e8a88c983
Rares <rares_lungescu@yahoo.com>
2021-01-04 19:19:46 +0200

    Write some tests for helper functions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import * as React from "react";
+import functions from "./components/Functions";
+import colors from "./colors";
+describe("GetProperty Helper Func", () => {
+  let testObj = { 1: 1, 2: "two", three: 3 };
+  test("Returns correct property", () => {
+    expect(functions.getProperty(testObj, 1)).toBe(1);
+    expect(functions.getProperty(testObj, 2)).toBe("two");
+    expect(functions.getProperty(testObj, "three")).toBe(3);
+  });
+});
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("AbsoluteValOfDifference Helper Func", () => {
+  test("Returns correct result when result is positive", () => {
+    expect(functions.absoluteValOfDifference(10, 5)).toBe(5);
+  });
+  test("Returns correct result when result is negative", () => {
+    expect(functions.absoluteValOfDifference(5, 10)).toBe(5);
+  });
+});
+
+describe("DetermineStringBeingTuned Helper Func", () => {
+  test("Returns correct string being tuned and color scheme for a tuning", () => {
+    expect(functions.determineStringBeingTuned("Standard", 82)).toStrictEqual({
+      currentStringBeingTuned: { letter: "E", frequency: 82 },
+      currentColors: colors.E,
+    });
+  });
+  test("Returns correct string being tuned and color scheme for a different tuning", () => {
+    expect(functions.determineStringBeingTuned("Drop D", 73)).toStrictEqual({
+      currentStringBeingTuned: { letter: "D", frequency: 73 },
+      currentColors: colors.D,
+    });
+  });
 });
