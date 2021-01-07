8d859c87457aadd3b220511ee36b3bd8ab582b21
Rares <rares_lungescu@yahoo.com>
2021-01-07 21:22:59 +0200

    Create Metronome UI

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -13,17 +13,27 @@ let Container = styled.div`
   height: 90%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  padding-top: 20%;
+  justify-content: space-around;
   align-items: center;
   margin-top: -150px;
+  // Remove additional styling from knob
   & div:nth-child(3) code {
     font-weight: 600;
     font-family: "Be Vietnam";
+  }
+  & div:nth-child(2) svg circle:first-child {
+    display: none;
+  }
+  & div:nth-child(2) svg svg {
+    display: none;
   }
 `;
 const Metronome: React.FunctionComponent<MetronomeProps> = ({
   primaryColor,
 }) => {
+  let [beatsPerMeasure, setBeatsPerMeasure] = React.useState(4);
+  let [tempo, setTempo] = React.useState(0);
   return (
     <Container>
       <CircularSlider
@@ -31,19 +41,25 @@ const Metronome: React.FunctionComponent<MetronomeProps> = ({
         labelColor={primaryColor}
         labelFontSize="0px"
         valueFontSize={"100px"}
-        verticalOffset={"2.5rem"}
+        verticalOffset={"2rem"}
         progressColorFrom={primaryColor}
         progressColorTo={primaryColor}
         progressSize={35}
         knobColor="#777777"
-        knobSize={70}
+        knobSize={85}
         trackColor="#96969633"
         trackSize={35}
         width={"340"}
         data={[...Array(180).keys()]} //...
         dataIndex={0}
+        onChange={(value: string) => {
+          setTempo(parseInt(value));
+        }}
       />
-      <MetronomeMeasureButton />
+      <MetronomeMeasureButton
+        beatsPerMeasure={beatsPerMeasure}
+        setBeatsPerMeasure={setBeatsPerMeasure}
+      />
     </Container>
   );
 };
--- src/components/MetronomeMeasureButtons.tsx
+++ src/components/MetronomeMeasureButtons.tsx
@@ -1,9 +1,101 @@
 import * as React from "react";
+import styled from "styled-components";
+import { wrap } from "comlink";
+import MetronomeLogic from "./MetronomeLogic.worker";
+let ColumnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+let RowContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+let MeasureIndicator = styled.div`
+  font-weight: 500;
+  font-size: 18px;
+  line-height:
+  color: #fff;
+  border-radius: 66px;
+  outline: none;
+  border: none;
+  padding: 10px 60px;
+  background-color: rgba(150, 150, 150, 0.2);
+  margin: 0px 10px;
+`;
+let StyledButton = styled.button`
+  font-weight: 900;
+  width: 10%;
+  font-size: 48px;
+  line-height: 48px;
+  color: #fff;
+  outline: none;
+  border: none;
+  background-color: transparent;
+  margin: 0px 10px;
+`;
+export interface MetronomeMeasureButtonsProps {
+  beatsPerMeasure: number;
+  setBeatsPerMeasure: React.Dispatch<React.SetStateAction<number>>;
+}
 
-export interface MetronomeMeasureButtonsProps {}
-
-const MetronomeMeasureButtons: React.FunctionComponent<MetronomeMeasureButtonsProps> = () => {
-  return <div>MetronomeMeasureButtons</div>;
+const myComlinkWorkerInstance: Worker = new MetronomeLogic();
+const myComlinkWorkerApi: any = wrap(myComlinkWorkerInstance);
+console.log(myComlinkWorkerApi.start);
+// let points = [];
+// let prev = 0;
+// let prevClockTime = 0;
+// let i = 0;
+// myComlinkWorkerApi.start((t: number) => {
+//   points.push(t - prev);
+//   points.push(myComlinkWorkerApi.audioCtx.currentTime - prevClockTime);
+//   prevClockTime = myComlinkWorkerApi.audioCtx.currentTime;
+//   i++;
+//   prev = t;
+// });
+console.log("[App] MyComlinkWorker instance:", myComlinkWorkerInstance);
+console.log("[App] MyComlinkWorker api:", myComlinkWorkerApi);
+// myComlinkWorkerApi.createMessage("John Doe").then((message: string): void => {
+//   console.log("[App] MyComlinkWorker message:", message);
+// });
+const MetronomeMeasureButtons: React.FunctionComponent<MetronomeMeasureButtonsProps> = ({
+  beatsPerMeasure,
+  setBeatsPerMeasure,
+}) => {
+  return (
+    <ColumnContainer>
+      <h2 style={{ textAlign: "center" }}>Beats</h2>
+      <RowContainer>
+        <StyledButton
+          style={{ marginTop: -10 }}
+          onClick={() => {
+            if (beatsPerMeasure > 1) setBeatsPerMeasure(beatsPerMeasure - 1);
+          }}
+        >
+          -
+        </StyledButton>
+        <MeasureIndicator>{beatsPerMeasure}</MeasureIndicator>
+        <StyledButton
+          onClick={() => {
+            if (beatsPerMeasure < 12) setBeatsPerMeasure(beatsPerMeasure + 1);
+          }}
+        >
+          +
+        </StyledButton>
+        <StyledButton
+          onClick={() => {
+            if (beatsPerMeasure < 12) setBeatsPerMeasure(beatsPerMeasure + 1);
+          }}
+        >
+          Start
+        </StyledButton>
+      </RowContainer>
+    </ColumnContainer>
+  );
 };
 
 export default MetronomeMeasureButtons;
