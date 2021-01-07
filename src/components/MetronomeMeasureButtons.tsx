a85affdf48eb66f81364ec1d5d7f0038f837c354
Rares <rares_lungescu@yahoo.com>
2021-01-07 16:06:05 +0200

    Create empty metronome control buttons component

---
+++ src/components/MetronomeMeasureButtons.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface MetronomeMeasureButtonsProps {}
+
+const MetronomeMeasureButtons: React.FunctionComponent<MetronomeMeasureButtonsProps> = () => {
+  return <div>MetronomeMeasureButtons</div>;
+};
+
+export default MetronomeMeasureButtons;
