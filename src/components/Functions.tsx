e68e9427c99c04b4582fdfff6f059c37923435dd
Rares <rares_lungescu@yahoo.com>
2021-01-13 02:24:50 +0200

    Create better function that handles the detection of string being tuned

--- src/components/Functions.tsx
+++ src/components/Functions.tsx
@@ -26,12 +26,6 @@ let functions = {
     let noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
     return noteStrings[(Math.round(noteNum) + 69) % 12];
   },
-  absoluteValOfDifference: function absoluteValOfDifference(
-    a: number,
-    b: number
-  ) {
-    return Math.abs(a - b);
-  },
   useAsyncEffect: function useAsyncEffect(
     fn: () => Promise<void | (() => void)>,
     dependencies?: React.DependencyList
@@ -43,6 +37,78 @@ let functions = {
       };
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, dependencies);
+  },
+
+  determineStringBeingTuned2: function determineStringBeingTuned(
+    tuning: string,
+    frequency: number,
+    buffer: Float32Array,
+    sampleRate: number
+  ) {
+    // Get the frequencies and letters for the specific tuning
+    let tuningFrequencies = functions.getProperty(
+      functions.getProperty(
+        Tunings,
+        tuning as "Standard" | "Drop D" | "Double Drop D" | "DADGAD"
+      ),
+      "frequencies"
+    );
+    let tuningLetters = functions.getProperty(
+      functions.getProperty(
+        Tunings,
+        tuning as "Standard" | "Drop D" | "Double Drop D" | "DADGAD"
+      ),
+      "letters"
+    );
+    let detectedFreq = frequency;
+    let closestFreq = 0;
+    let closestLetter = "-";
+    let minIndex = 0;
+    let minDifference = 9999;
+
+    // This way is much better, but still needs to be improved.
+    // It seems that when a sound seems to be the same note, but a different octave, the program will consider it to be the string being tuned,
+    // instead of the next note
+    Object.values(tuningFrequencies).map((stringFreq, index) => {
+      let offset = Math.round(sampleRate / stringFreq);
+
+      let difference = 0;
+      for (let i = 0; i < buffer.length / 2; i++) {
+        difference += Math.abs(buffer[i] - buffer[i + offset]);
+      }
+      difference /= buffer.length / 2;
+      if (
+        difference < minDifference &&
+        detectedFreq !== 0 &&
+        Math.abs(detectedFreq - stringFreq) < 70
+      ) {
+        minDifference = difference;
+        minIndex = index;
+      }
+      return null;
+    });
+    // Get the letter note closest to the one being tuned
+    closestLetter = functions.getProperty(
+      tuningLetters,
+      `string_${minIndex + 1}` as keyof Letters
+    );
+    // Get the expected frequency of the string being tuned
+    closestFreq = functions.getProperty(
+      tuningFrequencies,
+      `string_${minIndex + 1}` as keyof Frequencies
+    );
+    // Get the current color scheme based on the note letter
+    let currentColors = functions.getProperty(
+      colors,
+      closestLetter as keyof ColorSchemes
+    );
+    return {
+      currentStringBeingTuned: {
+        letter: closestLetter,
+        frequency: closestFreq,
+      },
+      currentColors: currentColors,
+    };
   },
   determineStringBeingTuned: function determineStringBeingTuned(
     tuning: string,
@@ -72,7 +138,7 @@ let functions = {
     // Map over the list of string frequencies
     Object.values(tuningFrequencies).map((stringFreq, index) => {
       // Compute the difference between the one detected, and the one known
-      let diff = functions.absoluteValOfDifference(detectedFreq, stringFreq);
+      let diff = Math.abs(detectedFreq - stringFreq);
       // If the diffrence is smaller
       if (diff < maxDifference && detectedFreq !== 0) {
         // We found the new minimum
